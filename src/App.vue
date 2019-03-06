b5ff78bfc7c0017636e7806d041e5598fd476a84
sclowbird <gregor.hlinka@gmail.com>
2019-03-06 19:41:36 +0100

    Initial commit.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NesLogin/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NesLogin from './components/NesLogin.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    NesLogin
   }
 }
 </script>
